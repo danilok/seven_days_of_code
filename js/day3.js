ac7c85597df94182d27abcc0d69a738033bc9b3f
Danilo Yorinori <daniloksy@gmail.com>
2022-04-02 16:17:04 -0300

    solution done

--- js/day3.js
+++ js/day3.js
@@ -40,120 +40,181 @@ async function desafioDia3() {
   });
 
   const question = util.promisify(rl.question).bind(rl);
+  const validAnswers = {
+    q1: ['frontend', 'front-end', 'back-end', 'backend', 'front', 'back'],
+    q2: {
+      F: ['react', 'vue'],
+      B:  ['c#', 'java']
+    },
+    q3: ['especializando', 'fullstack'],
+    q4: {
+      neg: ['nao', 'nenhuma', 'n'],
+      pos: ['sim', 'ok', 's'],
+      techs: ['c', 'c++', 'python', 'go', 'golang', 'node', 'docker', 'devops', 'html', 'css', 'angular']
+    }
+  }
 
+  /**
+   * 
+   * @returns F|B
+   */
   async function firstQuestion() {
-    const q1 = 'Se quer seguir para área de Front-End ou seguir para a área de Back-End? ';
+    const q1 = '1) Você quer seguir para área de Front-End ou seguir para a área de Back-End? ';
     const verifyAnswer = (answer) => {
-      const lower = answer.toLowerCase()
-      return ['frontend', 'front-end', 'back-end', 'backend', 'front', 'back'].includes(lower)
+      const lower = answer.toLowerCase();
+      const res = validAnswers.q1.includes(lower);
+      validateAnswer(res);
+      return res;
     }
-    let answer = await question(q1);
-    let acceptedAnwser = verifyAnswer(answer)
-    while (!acceptedAnwser) {
-      answer = await question(q1);
-      acceptedAnwser = verifyAnswer(answer);
-    }
+    let answer = await handleQuery(question, q1, verifyAnswer);
     return answer.startsWith('back') ? 'B' : 'F';
   }
   const answer1 = await firstQuestion();
-  console.log(answer1);
 
-  // async function questionExample() {
-  //   try {
-  //     const answer = await question('What is you favorite food? ');
-  //     console.log(`Oh, so your favorite food is ${answer}`);
-  //   } catch (err) {
-  //     console.error(err);
-  //     console.error('Question rejected', err);
-  //   }
-  // }
+  /**
+   * Caso esteja na área de Front-End, se quer aprender React ou aprender Vue.
+   * Caso esteja na área de Back-End, poderá aprender C# ou aprender Java.
+   * @returns F|B
+   */
+  async function secondQuestion(answer1) {
+    const q2 = getQuestion2(answer1)
+    const verifyAnswer = (answer, ...args) => {
+      const [area] = [...args];
+      const lower = answer.toLowerCase();
+      let res = validAnswers.q2[area].includes(lower);
+      validateAnswer(res);
+      return res;
+    }
+    let answer = await handleQuery(question, q2, verifyAnswer, answer1);
+    return answer;
+  }
+  const answer2 = await secondQuestion(answer1);
 
-  // (async () => await questionExample())();
+  /**
+   * Depois, independente das escolhas anteriores, o usuário poderá escolher 
+   * entre seguir se especializando na área escolhida ou seguir se desenvolvendo 
+   * para se tornar Fullstack. Você deve exibir na tela uma mensagem específica para cada escolha.
+   * @returns string
+   */
+  async function thirdQuestion(answer1, answer2) {
+    const q3 = getQuestion3(answer1, answer2)
+    const verifyAnswer = (answer) => {
+      const lower = answer.toLowerCase();
+      const res = validAnswers.q3.includes(lower);
+      validateAnswer(res);
+      return res;
+    }
+    let answer = await handleQuery(question, q3, verifyAnswer);
+    return answer;
+  }
+  const answer3 = await thirdQuestion(answer1, answer2);
+
+  async function fourthQuestion() {
+    const q4 = getQuestion4()
+    const verifyAnswer = (answer) => {
+      const lower = answer.toLowerCase();
+      const acceptedRange = validAnswers.q4.neg.concat(validAnswers.q4.techs);
+      const res = acceptedRange.includes(lower);
+      validateAnswer(res);
+      return res;
+    }
+    // let answers = await handleMultipleQueries(question, q4, verifyAnswer);
+    let answers = [];
+    let answer = await handleQuery(question, q4, verifyAnswer);
+    const abort = validAnswers.q4.neg.includes(answer);
+    if (abort) {
+      return [];
+    }
+    answers.push(answer);
+    
+    await handleMultipleQueries(question, validAnswers, answers);
+    return answers;
+  }
+  const answer4 = await fourthQuestion();
+
+  showSummary(answer1, answer2, answer3, answer4);
+
   rl.close();
 }
 
 (async () => await desafioDia3())();
 
-// import * as readline from 'node:readline/promises';
-// import { stdin as input, stdout as output } from 'process';
+function getQuestion2(answer1) {
+  switch (answer1) {
+    case 'F':
+      return '2) Na área de front-end, você quer aprender React ou aprender Vue? ';
+      case 'B':
+      return '2) Na área de back-end, você quer aprender C# ou aprender Java? ';
+  }
+}
 
-// const rl = readline.createInterface({ input, output });
+function getQuestion3(answer1, answer2) {
+  const area = answer1 === 'F' ? 'Front-end' : 'Back-end';
+  return `3) Após aprender ${answer2}, você deseja continuar se especializando na área de ${area}`
+    + ` ou seguir se desenvolvendo para ser tornar um Fullstack? `;
+}
 
-// const answer = await rl.question('What do you think of Node.js? ');
+function getQuestion4() {
+  return '4) Quais são as tecnologias nas quais você gostaria de se especializar ou de conhecer? ';
+}
 
-// console.log(`Thank you for your valuable feedback: ${answer}`);
+function getQuestion4More() {
+  return '4.1) Tem mais alguma tecnologia que você gostaria de aprender? ';
+}
 
-// rl.close();
+async function handleQuery(question, questionText, verifyAnswer, ...v) {
+  let answer = await question(questionText);
+  let acceptedAnswer = verifyAnswer(answer, ...v)
+  while (!acceptedAnswer) {
+    answer = await question(questionText);
+    acceptedAnswer = verifyAnswer(answer, ...v);
+  }
+  return answer;
+}
 
-//  rl.question('What is your name ? ', function (name) {
-//    rl.question('How old are you ? ', function (yearsOld) {
-//      rl.question('Which programming language are you studying ? ', function (programmingLanguage) {
-//        rl.question(`Do you like study ${programmingLanguage}? Anwser with number 1 for YES and 2 for NO ? `, function (answer) {
-//          console.log(`\nHello ${name}, you are ${yearsOld} years old and is already learning ${programmingLanguage}!`);
-//          if (answer === '1') {
-//            console.log('Very good! Keep studying and you\'ll have great success!');
-//          } else if (answer === '2') {
-//            console.log('How bad... Have you tried to learn other languages?');
-//          } else {
-//            console.log('Invalid answer! :(.')
-//          }
-//          rl.close();
-//        });
-//      });
-//    });
-//  });
- 
+async function handleKeepAsking(question, more, validAnswers) {
+  const verifyAnswer = (answer) => {
+    const lower = answer.toLowerCase();
+    const acceptedRange = validAnswers.q4.neg.concat(validAnswers.q4.pos);
+    const res = acceptedRange.includes(lower);
+    validateAnswer(res);
+    return res;
+  }
+  let answer = await handleQuery(question, more, verifyAnswer);
+  return validAnswers.q4.pos.includes(answer.toLowerCase());
+}
 
+async function handleMultipleQueries(question, validAnswers, answers) {
+  const verifyAnswer = (answer) => {
+    const lower = answer.toLowerCase();
+    const acceptedRange = validAnswers.q4.techs;
+    const res = acceptedRange.includes(lower);
+    validateAnswer(res);
+    return res;
+  }
+  
+  const q4 = getQuestion4()
+  const more = getQuestion4More();
+  while (await handleKeepAsking(question, more, validAnswers)) {
+    const answer = await handleQuery(question, q4, verifyAnswer);
+    answers.push(answer);
+  }
+}
 
-// const util = require('util');
-// const fs = require('fs');
+function showSummary(answer1, answer2, answer3, answer4) {
+  console.log('\nObrigado pelas respostas!! Vamos ao resumo:')
+  const area = answer1 === 'F' ? 'Front-end' : 'Back-end';
+  const future = answer3 === 'fullstack' ? answer3 : 'especialista';
+  console.log(`Você deseja seguir pela área de ${area} e aprender ${answer2}. No futuro, quer se tornar um ${future}.`);
+  if (answer4.length > 0) {
+    console.log(`Além disso, pretende estudar as seguintes tecnologias: ${answer4.join(',')}`)
+  } else {
+    console.log(`Por enquanto não pretende aprender nenhuma outra tecnologia.`)
+  }
+}
 
-// const stat = util.promisify(fs.stat);
-
-// async function callStat() {
-//   const stats = await stat('.');
-//   console.log(`This directory is owned by ${stats.uid}`);
-// }
-
-// callStat()
-
-// const util = require('util');
-// const question = util.promisify(rl.question).bind(rl);
-
-// async function questionExample() {
-//   try {
-//     const answer = await question('What is you favorite food? ');
-//     console.log(`Oh, so your favorite food is ${answer}`);
-//     rl.close();
-//   } catch (err) {
-//     console.error(err);
-//     console.error('Question rejected', err);
-//   }
-// }
-
-// (async () => await questionExample())();
-
-// function Ask(query) {
-//   const readline = require("readline").createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   })
-
-//   return  new Promise(resolve => readline.question(query, ans => {
-//   readline.close();
-//   resolve(ans);
-// }))
-// }
-
- 
-// // example useage
-// async function main() {
-
-//  var name = await Ask("whats you name")
-//  console.log(`nice to meet you ${name}`)
-
-//  var age = await Ask("How old are you?")
-//  console.log(`Wow what a fantastic age, imagine just being ${age}`)
-// }
-
-// main()
+function validateAnswer(res) {
+  if (!res) {
+    console.log('> Desculpe, mas esta resposta é inválida! :(');
+  }
+}
